df88e0e2d552f628ab45798e03aeb33023fd9903
Ryan Howarth <ryanhowarth4@gmail.com>
2015-07-15 15:55:29 -0400

    Made a seperate method for sending commands to the Dynamixel instead of being done in the ble receive method. Also, created a method to check the load at any time.

--- rfduino_hand_control.ino
+++ rfduino_hand_control.ino
@@ -14,54 +14,55 @@ int advertisement_led = 3;
 int connection_led = 2;
 
 // The pin to be used for enable/disable signal
-int txrx_pin= 4; 
+int txrx_pin = 4;
+
+// Debug pin
+int debug = 6;
+
+//Detmines whether the hand is currently closing.
+boolean closing =  false;
 
 void setup() {
-  
+
   //Change the advertisement Interval to be small
   int interval = 20;
   RFduinoBLE.advertisementInterval = interval;
-  
+
   // led used to indicate that the RFduino is advertising
   pinMode(advertisement_led, OUTPUT);
-  
+
   // led used to indicate that the RFduino is connected
   pinMode(connection_led, OUTPUT);
-  
-  delay(500); //Let dynamixel start up 
-  
+
+  delay(500); //Let dynamixel start up
+
   // Pin to control direction of information (TX/RX)
   pinMode(txrx_pin, OUTPUT);
   digitalWrite(txrx_pin, LOW);
-  
+
   //Debugging Pin
-  pinMode(6, OUTPUT);
-  
+  pinMode(debug, OUTPUT);
+
   //Start Serial Connection
   Serial.begin(9600);
-  
+
   // start the BLE stack
   RFduinoBLE.begin();
+
+
 }
 
 void loop() {
   //No delay (Constantly Looping)
   RFduino_ULPDelay(0);
- 
+
   //Reads commands from Dynamixel and sends them over BLE to Laptop
   if (Serial.available() > 0) {
-    digitalWrite(txrx_pin, LOW); //Switch to receive data from dynamixel 
+    digitalWrite(txrx_pin, LOW); //Switch to receive data from dynamixel
     char testRead[8] = {};
     //char data = testRead[0];
     Serial.readBytes(testRead, 8);
-    
-    /*for (int j = 0; j < sizeof(testRead); j++) {
-      int f  = int(testRead[j]);
-      if (f == 1) {
-        data = testRead[j+3];
-      }
-    }*/
-    
+    get_value(testRead);
     RFduinoBLE.send(testRead, 8);
   }
 
@@ -71,11 +72,27 @@ void loop() {
   //RFduino_ULPDelay( SECONDS(1) );
 }
 
+int get_value(char *dataRead)
+{
+  int servo_id = 1;
+  for (int i = 0; i < sizeof(dataRead) / sizeof(char); i++) {
+    int value  = int(dataRead[i]);
+    if (value == servo_id) {
+      int data = int(dataRead[i + 3]);
+      if (data > 20 && data < 30) {
+        //digitalWrite(debug, HIGH);
+      }
+      return data;
+    }
+  }
+  return -1;
+}
+
 void RFduinoBLE_onAdvertisement(bool start)
 {
   // turn the green led on if we start advertisement, and turn it
   // off if we stop advertisement
-  
+
   if (start)
     digitalWrite(advertisement_led, HIGH);
   else
@@ -97,17 +114,49 @@ void RFduinoBLE_onDisconnect()
 */
 void RFduinoBLE_onReceive(char *data, int len)
 {
-  
-   digitalWrite(txrx_pin, HIGH); //Switch to TX mode before sending data
-   delay(10);                    //Allow this to take effect
-   uint8_t a = data[0];
-   for (int i = 0; i < len; i++) {
-     byte hex_byte = '\x00' + int(data[i]);
-     delay(1); // Need spacing between bytes for some reason?
-     Serial.write(hex_byte);
-   }
-   delayMicroseconds(1200); //Allow last bit to go through before switching to RX (at 9600 baud)
-   digitalWrite(txrx_pin, LOW);
+
+  uint8_t a = data[0];
+  byte hex_byte[len];
+  for (int i = 0; i < len; i++) {
+    hex_byte[i] = '\x00' + int(data[i]);
+
+  }
+  if (int(data[5]) == 40) {
+    digitalWrite(debug, HIGH);
+  }
+  else
+  {
+    send_to_servo(hex_byte, len);
+  }
+
+  /*
+  digitalWrite(txrx_pin, HIGH); //Switch to TX mode before sending data
+  delay(10);                    //Allow this to take effect
+  for (int i = 0; i < len; i++) {
+    byte hex_byte = '\x00' + int(data[i]);
+    delay(1); // Need spacing between bytes for some reason?
+    Serial.write(hex_byte);
+  }
+  delayMicroseconds(1200); //Allow last bit to go through before switching to RX (at 9600 baud)
+  digitalWrite(txrx_pin, LOW);*/
 }
-  
+
+void send_to_servo(byte *data, int len)
+{
+  digitalWrite(txrx_pin, HIGH); //Switch to TX mode before sending data
+  delay(10);                    //Allow this to take effect
+  for (int i = 0; i < len; i++) {
+    delay(1); // Need spacing between bytes for some reason?
+    Serial.write(data[i]);
+  }
+  delayMicroseconds(1200); //Allow last bit to go through before switching to RX (at 9600 baud)
+  digitalWrite(txrx_pin, LOW);
+}
+
+void check_load()
+{
+  byte load_packet[] = {'\xff', '\xff', '\x01', '\x04', '\x02', '\x28', '\x02', '0xCE'};
+  send_to_servo(load_packet, sizeof(load_packet) / sizeof(byte) );
+}
+
 
